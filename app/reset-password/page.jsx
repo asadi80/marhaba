ac4985a2cc8707315b718777cbcd26d824e98f60
Abdurraouf <a.k.sadi80@gmail.com>
2026-05-08 18:19:07 -0700

    forgot password

--- app/reset-password/page.jsx
+++ app/reset-password/page.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
-import "./style.css";
+// import "./style.css";
 
 function ResetPasswordContent() {
   const searchParams = useSearchParams();
