9a13cb1f201d7c86f4b320fd3f47bdc25dbb9c51
Abdurraouf <a.k.sadi80@gmail.com>
2026-06-05 12:07:20 -0700

    updated logo

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
-/** @type {import('next').NextConfig} */
 import withSerwist from "@serwist/next";
+
+const withPWA = withSerwist({
+  swSrc: "app/sw.js",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig = {
   reactStrictMode: true,
@@ -10,52 +15,5 @@ const nextConfig = {
     },
   },
 };
-const withPWA = withSerwist({
-  swSrc: "app/sw.js",      // you write the SW
-  swDest: "public/sw.js",
-  disable: process.env.NODE_ENV === "development",
-});
 
-
-export default withPWA({
-  dest: 'public',
-  reactStrictMode: true,
-  register: true,
-  skipWaiting: true,
-  cacheStartUrl: true,
-  dynamicStartUrl: true,
-  workboxOptions: {
-   
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'google-fonts',
-          expiration: {
-            maxEntries: 30,
-            maxAgeSeconds: 60 * 60 * 24 * 365,
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/.*\.tile\.openstreetmap\.org\/.*/i,
-        handler: 'NetworkOnly',
-        options: {
-          cacheName: 'osm-tiles',
-        },
-      },
-      {
-        urlPattern: /^\/api\/.*/i,
-        handler: 'NetworkOnly',
-        options: {
-          cacheName: 'api-responses',
-        },
-      },
-    ],
-  },
-  ...(process.env.NODE_ENV === 'development' && {
-    onSuccess: undefined,
-    onRegister: undefined,
-  }),
-})(nextConfig);
+export default withPWA(nextConfig);
